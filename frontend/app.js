2b108b900d52c650d71ce0069368c88a3ff733d6
Eze14Mu <154024777+Eze14Mu@users.noreply.github.com>
2024-12-26 17:43:47 -0800

    Delete frontend directory

--- frontend/app.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('checkStatus').addEventListener('click', async () => {
-    const response = await fetch('/api/status');
-    const data = await response.json();
-    document.getElementById('statusOutput').innerText = data.status;
-});
